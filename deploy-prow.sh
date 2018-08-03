02d98d4f0f54332075c1d2b14d1c2545a0750624
hemanik <hemanikatyal@gmail.com>
2018-08-03 23:31:38 +0530

    updated the configs and tests

--- deploy-prow.sh
+++ deploy-prow.sh
@@ -5,4 +5,5 @@ set -x
 kubectl create secret generic hmac-token --from-file=hmac=tokens/hmac.token
 kubectl create secret generic oauth-token --from-file=oauth=tokens/oauth.token
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/test-infra/master/prow/cluster/starter.yaml
+kubectl apply -f \ 
+https://raw.githubusercontent.com/kubernetes/test-infra/master/prow/cluster/starter.yaml
--- update-configs.sh
+++ update-configs.sh
@@ -8,8 +8,5 @@ kubectl create configmap plugins \
 
 
 kubectl create configmap config \
-  --from-file=config.yaml=config.yaml --dry-run -o yaml | kubectl replace configmap config -f -
-
-kubectl scale deploy hook --replicas=0
-sleep 1
-kubectl scale deploy hook --replicas=1
+  --from-file=config.yaml=config.yaml --dry-run -o yaml \
+  | kubectl replace configmap config -f -
